a1f8f149738f7d769141b920c940e1e0af075cfc
Sol Lee <u103133.u103135@gmail.com>
2022-07-10 03:58:01 +0000

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # ckanext-ark CHANGELOG
 
 
+## v1.0.2 - 2022-07-10
+
+- Fix that the update-ark command does not update erc metadata
+
 ## v1.0.1 - 2022-07-07
 
 - Noid: mint arks with betanumeric extended-digits
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ckanext-ark',
-    version='1.0.1',
+    version='1.0.2',
     description='A CKAN extension for assigning Archival Resource Key '
                 '(ARK) identifiers to datasets.',
     long_description=long_description,
